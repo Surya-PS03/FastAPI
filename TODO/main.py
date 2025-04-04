e6530e23d4ed8ea10709c7721fefc8751a24c21e
Surya-PS03 <suryapratapsingh461989@gmail.com>
2025-04-04 19:38:29 +0530

    added read a single todo api endpoint with path paramter validation, detail of an error in case of an exception raised and explicitly mentioning if the code ran it has status code 200

--- TODO/main.py
+++ TODO/main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,HTTPException,Path
 from sqlalchemy.orm import Session
 from models import Base
 from database import engine,SessionLocal
-from typing import Annotated
 from models import Todo
+from starlette import status
 
 app = FastAPI()
 
@@ -15,6 +15,17 @@ def get_db():
     finally:
         db.close()
 
-@app.get('/')
+@app.get('/todos')
 async def read_all(db : Session = Depends(get_db)):
     return db.query(Todo).all()
+
+@app.get('/todos/{todo_id}',status_code  = status.HTTP_200_OK)
+
+async def get_todo(db: Session = Depends(get_db), todo_id: int = Path(gt=0)):
+
+    todo_model = db.query(Todo).filter(Todo.id == todo_id).first()
+
+    if todo_model is not None:
+        return todo_model
+
+    raise HTTPException(status_code = 404, detail = "Todo not found")
