c5d004a5b9dc266ed2bf39214415edbbe1a12421
Surya-PS03 <suryapratapsingh461989@gmail.com>
2025-04-04 23:43:50 +0530

    added a post todo request

--- TODO/main.py
+++ TODO/main.py
@@ -4,6 +4,7 @@ from models import Base
 from database import engine,SessionLocal
 from models import Todo
 from starlette import status
+from pydantic import BaseModel,Field
 
 app = FastAPI()
 
@@ -15,7 +16,12 @@ def get_db():
     finally:
         db.close()
 
-@app.get('/todos')
+class TodoRequest(BaseModel):
+    title : str = Field(min_length = 1)
+    priority : int = Field(lt = 5,gt=0)
+    status: bool
+
+@app.get('/todos', status_code=status.HTTP_200_OK)
 async def read_all(db : Session = Depends(get_db)):
     return db.query(Todo).all()
 
@@ -29,3 +35,15 @@ async def get_todo(db: Session = Depends(get_db), todo_id: int = Path(gt=0)):
         return todo_model
 
     raise HTTPException(status_code = 404, detail = "Todo not found")
+
+
+@app.post('/todos/create_todos',status_code = status.HTTP_201_CREATED)
+async def create_todo(todo_request: TodoRequest, db: Session = Depends(get_db)):
+    todo_model = Todo(**todo_request.model_dump())
+
+    db.add(todo_model)
+
+    db.commit()
+
+
+
